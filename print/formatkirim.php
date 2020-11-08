e1f131fde0138593a69a9014db6051b193c57a81
adji142 <prasetyoajiw@gmail.com>
2020-11-08 23:09:19 +0700

    add post feture 28

--- print/formatkirim.php
+++ print/formatkirim.php
@@ -1,7 +1,7 @@
 <?php
 //buka koneksi ke engine MySQL
-    $Open = mysqli_connect("localhost","root","hsp123","dbpos");
-    // $Open = mysqli_connect("localhost","aistrick_root","lagis3nt0s4","aistrick_xlpfk_solo");
+    // $Open = mysqli_connect("localhost","root","hsp123","dbpos");
+    $Open = mysqli_connect("localhost","aisx1277_root","lagis3nt0s4","aisx1277_dbpos");
     // mysqli_connect("localhost","root","hsp123","xlpfk_solo");
     // $Open = mysqli_connect("localhost","root","lagis3nt0s4","dealsys");
     if (!$Open){
