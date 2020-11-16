f62f0b1a235a03106fd4ee9ed9a216a375de6b83
adji142 <prasetyoajiw@gmail.com>
2020-11-16 19:57:50 +0700

    add post feture 32.1

--- application/controllers/C_POS.php
+++ application/controllers/C_POS.php
@@ -125,7 +125,7 @@ class C_POS extends CI_Controller {
 				ROUND(a.T_GrandTotal) T_GrandTotal
 			FROM penjualanheader a
 			LEFT JOIN tcustomer c on a.KodeCustomer = c.KodeCustomer
-			WHERE A.NoTransaksi = '".$NoTransaksi."'
+			WHERE a.NoTransaksi = '".$NoTransaksi."'
 		";
 
 		$rs = $this->db->query($SQL);
